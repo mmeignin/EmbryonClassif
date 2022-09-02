93ca712f2c0602c44bf36b9424f038485de85b3f
Matthieu Meignin <mmeignin@slurm-maiage.inrae.fr>
2022-09-02 11:31:26 +0200

    run config

--- Build/run_1.sh
+++ Build/run_1.sh
@@ -14,8 +14,8 @@ if [ ${VIRTUAL_ENV:(-7)} == "classif" ]; then
         #pip install -r "$(pwd)/requirements.txt"
         cd ..
         python3 training.py  \
-                                --data_file EmbryonBinary_RandomSplit\
-                                -bb ResNet18 -he ConvPooling\
+                                --data_file Embryon_RS_fixed_val\
+                                -bb ResNet18 -he Gru\
                                 --criterion_name bce_balanced\
                                 --augmentation hflip vflip randombrightness fill_background\
                                 --framestep 4\
